e72da8cafcf0533952e3476007b0281a308fa594
chpr77 <43355619+chpr77@users.noreply.github.com>
2020-09-23 01:20:35 +0530

    Update PersonController.java

--- src/main/java/com/javatpoint/controller/PersonController.java
+++ src/main/java/com/javatpoint/controller/PersonController.java
@@ -6,15 +6,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javatpoint.model.Books;
 import com.javatpoint.model.Person;
-import com.javatpoint.model.PersonAddress;
-import com.javatpoint.service.BooksService;
-import com.javatpoint.service.PersonAddressService;
 import com.javatpoint.service.PersonService;
 //mark class as Controller
 @RestController
@@ -22,11 +17,7 @@ public class PersonController
 	{
 	
 	@Autowired
-	PersonService personService;
-	
-	@Autowired
-	PersonAddressService personAddressService;
-	
+	PersonService personService;	
 	
 	
 	@GetMapping("/allPersons")
@@ -53,28 +44,7 @@ public class PersonController
 	private void deletePerson(@PathVariable("personId") int personId) 
 	{
 		personService.delete(personId);
-	}
-	
-	//Address 
-	
-	@GetMapping("/AllpersonAddress")
-	private List<PersonAddress> getAllPersonsAddress() 
-	{
-		return personAddressService.getAllPersonsAddress();
-	}
-	
-	@PostMapping("/savePersonAddress")
-	private int savePerson(@RequestBody PersonAddress personAddress) 
-	{
-		personAddressService.saveOrUpdate(personAddress);
-		return personAddress.getAddressId();
-	}
-	
-	@DeleteMapping("/deletePersonAddress/{addressId}/{personId}")
-	private void deletePersonAddress(@PathVariable("addressId") int id,@PathVariable("personId") int personId) 
-	{
-		personAddressService.delete(id, personId);
-	}
+	}	
 	
 	
 }
